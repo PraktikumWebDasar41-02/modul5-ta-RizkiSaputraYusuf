c825ea6f886c87f0e0509ea056ee3749bc0299d9
RizkiSaputraYusuf <42662979+RizkiSaputraYusuf@users.noreply.github.com>
2018-11-01 09:18:26 +0700

    Add files via upload

---
+++ data1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form action="" method="POST">
+	<center>
+	<h1> Masukan Data Paket</h1>
+	Nama<input type="text" name="name">
+	Nip<input type="text" name="name">
+
+	<input type="submit" name="submit" value="send">
+</center>
+</body>
+</form>
+
+</body>
+</html>
---
+++ karyawan.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form action="" method="POST">
+	<center>
+	<h1> Masukan Data</h1>
+	Tanggal Datang : <input type="text" name="tanggaldatang">
+	Sasaran: <input type="text" name="penerima">
+	IsiPaket:<input type="text" name="IsiPaket">
+	Tanggal Ambil:<input type="text" name="tanggalambil">
+
+	<input type="submit" name="submit" value="send">
+</center>
+</body>
+</form>
+
+</body>
+</html>
